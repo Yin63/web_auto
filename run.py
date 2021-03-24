6e38dd5332befc72b3ac0f74af8c88ca93059c1c
Yni63 <2087104694@qq.com>
2021-03-24 19:29:38 +0800

    test

--- run.py
+++ run.py
@@ -7,3 +7,5 @@ driver.implicitly_wait(8)
 results = Python_auto.begin_run(driver, test_data.url['url'], test_data.userinfo['username'], test_data.userinfo['password'], test_data.num['num'])
 
 print(results)
+
+print('测试')
